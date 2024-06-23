3ec6ee15bfe7a4c0aaeed6cc50c3374d2eb6c1c9
Jayesh More <65273915+jayesh-more@users.noreply.github.com>
2024-06-23 17:37:02 +0530

    Update fastapi_backend.py

--- fastapi_backend.py
+++ fastapi_backend.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.responses import JSONResponse
-import whisper # type: ignore
+import whisper 
 import tempfile
 import os
 
@@ -10,22 +10,20 @@ model = whisper.load_model("base")
 @app.post("/transcribe/")
 async def transcribe(file: UploadFile = File(...)):
     try:
-        # Save the uploaded audio file to a temporary file
+        # Save uploaded audio file a temporary file
         with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_file:
             temp_file.write(file.file.read())
             temp_file_path = temp_file.name
 
-        # Transcribe the audio file with timestamps
+        # Transcribe audio file with timestamps
         result = model.transcribe(temp_file_path, language="en", verbose=True)
 
-        # Remove the temporary audio file
+        # Remove temporary audio file
         os.remove(temp_file_path)
 
-        # Return the transcription and timestamps
         return JSONResponse(content={"transcription": result["text"], "segments": result["segments"]})
 
     except Exception as e:
-        # Log the error and return an appropriate message
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
 
